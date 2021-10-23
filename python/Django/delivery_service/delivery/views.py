b4b11b6800834a34a0abb81216ebd44975dc7165
nym0101 <nym0101@echomarketing.co.kr>
2021-10-23 17:27:40 +0900

    [Prac] study python django

--- python/Django/delivery_service/delivery/views.py
+++ python/Django/delivery_service/delivery/views.py
@@ -10,16 +10,9 @@ def order_list(request, shop):
     if request.method == 'GET':
         order_list = Order.objects.filter(shop=shop)
         return render(request, 'boss/order_list.html', {'order_list':order_list})
-    else:
-        return HttpResponse(status=404)
-
-@csrf_exempt    
-def time_input(request):
-    if request.method == "POST":
+    elif request.method == "POST":
         order_item = Order.objects.get(pk=int(request.POST['order_id']))
         shop = order_item.shop.id
         order_item.estimated_time = int(request.POST['estimated_time'])
         order_item.save()
         return render(request, 'boss/success.html', {'shop':int(shop)})
-    else:
-        return HttpResponse(status=404)
